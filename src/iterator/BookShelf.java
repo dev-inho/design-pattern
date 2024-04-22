eabaaff44fc56d37b72b0d6f7f151c5da59cf839
jinsung <201519011@yiu.ac.kr>
2024-04-22 21:00:36 +0900

    Iteartor 패턴 구현

---
+++ src/iterator/BookShelf.java
@@ -0,0 +1,32 @@
+package iterator;
+
+import java.util.Iterator;
+
+public class BookShelf implements Iterable<Book> {
+
+    private final Book[] books;
+    private int last = 0;
+
+    public BookShelf(int maxSize) {
+        this.books = new Book[maxSize];
+    }
+
+    public Book getBookAt(int index) {
+        return books[index];
+    }
+
+    public void appendBook(Book book) {
+        this.books[last] = book;
+        this.last++;
+    }
+
+    public int length() {
+        return this.last;
+    }
+
+    @Override
+    public Iterator<Book> iterator() {
+        return new BookShelfIterator(this);
+    }
+
+}
